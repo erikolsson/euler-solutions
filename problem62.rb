0e5b7e692233388950f168874aa3985a72cce558
Erik Olsson <valross2@gmail.com>
2013-05-04 14:42:19 +0200

    solved 62

---
+++ problem62.rb
@@ -0,0 +1,31 @@
+
+
+class CubePermutations
+	attr_accessor :permutations
+	attr_accessor :smallest_n
+	def initialize(n)
+		@smallest_n = n
+		@permutations = 0
+	end
+end
+
+cubes = Hash.new
+
+def largest_permutation (n)
+	n.to_s.chars.sort.reverse
+end
+
+i = 1
+while true
+	cube = i * i * i
+	largest = largest_permutation(cube)
+	cubes[largest] ||= CubePermutations.new(i)
+	cubes[largest].permutations += 1
+
+	if cubes[largest].permutations == 5
+		puts "the smallest cube: #{cubes[largest].smallest_n}"
+		break
+	end
+
+	i+=1
+end
