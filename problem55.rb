448b7f5e0153813f1a2c1cb36dc42bb8043255d9
Erik Olsson <valross2@gmail.com>
2013-05-10 12:20:23 +0200

    fix

--- problem55.rb
+++ problem55.rb
@@ -1,14 +1,14 @@
 
 def lychrel(n, level)
-	return -1 if level > 50
+	return true if level > 50
 	candidate = n + n.to_s.reverse.to_i
-	return level - 1 if candidate.to_s == candidate.to_s.reverse
+	return false if candidate.to_s == candidate.to_s.reverse
 	lychrel(candidate, level + 1)
 end
 
 total = 0
 for i in 1..9999
-	total += 1 if lychrel(i, 1) == -1
+	total += 1 if lychrel(i, 1)
 end
 
 puts "Total: #{total}"
