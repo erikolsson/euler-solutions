98a43cd8760da33d0b8f68316c8d116a79821c4a
Erik Olsson <valross2@gmail.com>
2013-05-12 22:43:36 +0200

    update 99

--- problem99.rb
+++ problem99.rb
@@ -1,18 +1,8 @@
 values = File.read("./problem99_data.txt").split("\n").map{|line| line.split(",").map{|v| v.to_i}}
 
-largest = 0.0
-largest_line = 0
-i = 1
+largest = values.each_with_index.map {|line, i|
+	base, exp = line[0], line[1]
+	[exp * Math.log(base), i+1]
+}.max[1]
 
-values.each do |line|
-	base = line[0]
-	exp = line[1]
-	digits = exp * Math.log(base)
-	if digits > largest
-		largest_line = i
-		largest = digits
-	end
-	i+=1
-end
-
-puts "Line: #{largest_line}"
+puts "Largest line: #{largest}"
